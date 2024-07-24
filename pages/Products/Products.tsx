9822e5d7e4c0a027733fc7fe0ebd04c3f35baa8a
gulsemins <gulsemin111@gmail.com>
2024-07-24 13:47:14 +0300

    initial commit

---
+++ pages/Products/Products.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./Products.style";
+
+const Products = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Products</Text>
+    </View>
+  );
+};
+
+export default Products;
